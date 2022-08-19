5720e6c8db216c33fe964334c0f7b5a51a0d63fb
SURAJ KUMAR SHARMA <75290099+sharma62@users.noreply.github.com>
2022-08-19 17:09:44 +0530

    Update Bubble sort
    Here is code & Explanation

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -1,40 +1,107 @@
-#include<bits/stdc++.h>
-using namespace std;
+/*
+Logic :
+------------------------------------------------------------------------------------------------
+pass:1
+    * Compare arr[1] and arr[2] , if(arr[1] > arr[2]) then swap.
+    * Compare arr[2] and arr[3] , if(arr[2] > arr[3]) then swap.
+    * Compare arr[0] and arr[1] , if(arr[0] > arr[1]) then swap.
+    ------------------------------
+    ------------------------------
+    ------------------------------
 
+    * Compare arr[n-2] and arr[n-1] , if(arr[n-2] > arr[n-1]) then swap.
+    Ruselt : Largest element is plased at (n-1)th position
+        arr[n-1] is sorted.
+------------------------------------------------------------------------------------------------
+
+pass:2
+    * Compare arr[1] and arr[2] , if(arr[1] > arr[2]) then swap.
+    * Compare arr[2] and arr[3] , if(arr[2] > arr[3]) then swap.
+    * Compare arr[0] and arr[1] , if(arr[0] > arr[1]) then swap.
+    * ------------------------------
+    * ------------------------------
+    * ------------------------------
+    * Compare arr[n-3] and arr[n-2] , if(arr[n-3] > arr[n-2]) then swap.
+    Ruselt : 2nd Largest element is plased at (n-2)th position
+      arr[n-2] ,  arr[n-1] is sorted.
+------------------------------------------------------------------------------------------------
+
+pass:3
+    * Compare arr[1] and arr[2] , if(arr[1] > arr[2]) then swap.
+    * Compare arr[2] and arr[3] , if(arr[2] > arr[3]) then swap.
+    * Compare arr[0] and arr[1] , if(arr[0] > arr[1]) then swap.
+    *  ------------------------------
+    *  ------------------------------
+    *  ------------------------------
+    * Compare arr[n-4] and arr[n-3] , if(arr[n-4] > arr[n-3] ) then swap.
+    Ruselt : 3rd Largest element is plased at (n-3)th position
+      arr[n-3] , arr[n-2] ,  arr[n-1] is sorted.
+
+pass:4
+    -------------------
+pass:5
+    -------------------
+pass:6
+    -------------------
+
+
+------------------------------------------------------------------------------------------------
+pass:n-2
+    * Compare arr[1] and arr[2] , if(arr[1] > arr[2]) then swap.
+    * Compare arr[2] and arr[3] , if(arr[2] > arr[3]) then swap.
+    Result : arr[2].....arr[n-2],arr[n-1] are sorted.
+
+------------------------------------------------------------------------------------------------
+pass:n-2
+    * Compare arr[1] and arr[2] , if(arr[1] > arr[2]) then swap.
+
+     Result :arr[1], arr[2].....arr[n-2],arr[n-1]  are sorted.
+
+ Note : arr[0] get automatically its right position.
+
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+#define MAX 100
 int main()
 {
-    int num=5;
-    int arr[5]={5,3,8,6,1};
-
-    for (size_t i = 0; i < num; i++)
+    int arr[MAX], i, j, temp, n, xchange;
+    cout << "Enter the number of element" << endl;
+    cin >> n;
+    for (i = 0; i < n; i++)
     {
-        cout << " "<< arr[i] ;
+        cout << "Enter element " << i << endl;
+        cin >> arr[i];
     }
-    cout << endl;
-    
-//  Logic of bubble sort .
-    int swap =0;
 
-    for (size_t i = 0; i < num - 1; i++)
+    // bubble sorting
+
+    for (i = 0; i < n; i++)
     {
-        cout<<"pass i: "<<i<<endl;
- 
-        for (size_t j = 0; j < num -i-1 ; j++)
+        xchange = 0;
+        for (j = 0; j < n - i; j++)
         {
-        cout<<"j: "<<j<<endl;
-
-            if (arr[j] < arr[j + 1])
+            if (arr[i] > arr[j + 1])
             {
-              int temp = arr[j];
-              arr[j]= arr[j+1];
-              arr[j+1]= temp ;
- 
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                xchange++;
             }
         }
+        if (xchange == 0)
+            break;
     }
 
-    for (size_t i = 0; i < num; i++)
+    cout << "sorted list" << endl;
+    for (i = 0; i < n; i++)
     {
-        cout<< " " << arr[i] ;
+        cout << arr[i] << endl;
     }
+
+    return 0;
 }
